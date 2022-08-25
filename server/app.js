4e821c27bbf17788698b9196432f821de94d9d2a
barjames12 <barjames7@gmail.com>
2022-08-25 14:22:02 +0300

    server update

--- server/app.js
+++ server/app.js
@@ -1,43 +1,44 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import dotenv from "dotenv";
-// import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
+import * as users from "./controllers/users.js";
+import * as vacationsControllers from "./controllers/vacationsControllers.js";
+import * as followControllers from "./controllers/followController.js";
 
-const express = require("express");
-const dotenv = require("dotenv")
-const bodyParser = require("body-parser")
-const cors = require("cors")
+import * as errorHandler from './errors/error-handler.js';
 
-// import * as users from "./controllers/users.js";
-// import * as vacationsControllers from "./controllers/vacationsControllers.js";
-// import * as followControllers from "./controllers/followController.js";
-
-// import * as errorHandler from './errors/error-handler.js';
-// import * as loginFilter from "./middleware/login-filter.js";
-
-
-const port = process.env.PORT || 3001;
 const server = express();
-server.use(express.json());
-
 dotenv.config();
 server.use(bodyParser.json({ limit: "30mb", extended: true }));
 server.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+server.use(cors()); //react
 
-const users = require("./controllers/users.js");
-const vacationsControllers = require("./controllers/vacationsControllers.js");
-const followControllers = require("./controllers/followController.js");
+// const express = require("express");
+// const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+// const cors = require("cors")
+
+// import * as loginFilter from "./middleware/login-filter.js";
+
+
+const PORT = process.env.PORT || 3001;
+server.use(express.json());
+
+
+// const users = require("./controllers/users.js");
+// const vacationsControllers = require("./controllers/vacationsControllers.js");
+// const followControllers = require("./controllers/followController.js");
 
 
 
 
 
-const errorHandler = require("./errors/error-handler.js");
-const loginFilter = require("./middleware/login-filter.js");
+// const errorHandler = require("./errors/error-handler.js");
+// const loginFilter = require("./middleware/login-filter.js");
 
 // server.use(cors({ origin: "http://localhost:3000" })); //react
-server.use(cors()); //react
 // server.use(cors({ origin: "http://localhost:4200" })); //angular
 
 // server.use(loginFilter());
@@ -53,6 +54,6 @@ server.get("/", (req, res) => {
 });
 
 
-server.listen(port, () => {
-  console.log(`server running on ${port}`);
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
 });
